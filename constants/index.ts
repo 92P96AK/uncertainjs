1bbe53cb074029530e7dba334e3c9d173a75e8f5
pradip <pradip.kharal777@gmail.com>
2024-04-30 02:22:59 +0545

    randomImageUrls

--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,4 @@ export const LOWER_CASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
 export const UPPER_CASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const NUMERIC_CHARS = "0123456789";
 export const SPECIAL_CHARS = "!@#$%^&*()_+{}[];:<>,.?/~";
+export const randomImageUrls = ["https://picsum.photos"];
