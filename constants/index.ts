2d30b8360f133864979a8aebbda9abef2fe79436
pradip <pradip.kharal777@gmail.com>
2024-05-02 01:44:52 +0545

    export

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,4 @@
+export * from './nouns_verbs_adjectives'
 export const LOWER_CASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
 export const UPPER_CASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const NUMERIC_CHARS = "0123456789";
