cccc300419d7bbc862e2ebf90ac8c3d00a3c88d5
pradip <pradip.kharal777@gmail.com>
2024-05-02 01:39:24 +0545

    consonants, characters added

--- constants/index.ts
+++ constants/index.ts
@@ -4,3 +4,5 @@ export const NUMERIC_CHARS = "0123456789";
 export const SPECIAL_CHARS = "!@#$%^&*()_+{}[];:<>,.?/~";
 export const randomImageUrls = ["https://picsum.photos"];
 export const VOWELS = 'aeiou'
+export const CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
+export const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz0123456789'
