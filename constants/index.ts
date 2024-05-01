10532f450ec6dcd4d0409718d837d52cdb0b57aa
pradip <pradip.kharal777@gmail.com>
2024-05-02 01:37:03 +0545

    vowels added

--- constants/index.ts
+++ constants/index.ts
@@ -3,3 +3,4 @@ export const UPPER_CASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const NUMERIC_CHARS = "0123456789";
 export const SPECIAL_CHARS = "!@#$%^&*()_+{}[];:<>,.?/~";
 export const randomImageUrls = ["https://picsum.photos"];
+export const VOWELS = 'aeiou'
