b17badd81c76ffaf75f77805b0272727d7c72e9f
pradip <pradip.kharal777@gmail.com>
2024-05-03 00:08:37 +0545

    DISPOSABLE_EMAIL_PROVIDER added

--- constants/index.ts
+++ constants/index.ts
@@ -1,9 +1,33 @@
-export * from './nouns_verbs_adjectives'
+export * from "./nouns_verbs_adjectives";
 export const LOWER_CASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
 export const UPPER_CASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const NUMERIC_CHARS = "0123456789";
 export const SPECIAL_CHARS = "!@#$%^&*()_+{}[];:<>,.?/~";
 export const randomImageUrls = ["https://picsum.photos"];
-export const VOWELS = 'aeiou'
-export const CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
-export const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz0123456789'
+export const VOWELS = "aeiou";
+export const CONSONANTS = "bcdfghjklmnpqrstvwxyz";
+export const CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789";
+export const DISPOSABLE_EMAIL_PROVIDER = [
+  "mailinator.com",
+  "sharklasers.com",
+  "guerrillamail.info",
+  "grr.la",
+  "guerrillamail.biz",
+  "guerrillamail.com",
+  "guerrillamail.de",
+  "guerrillamail.net",
+  "guerrillamail.org",
+  "guerrillamailblock.com",
+  "pokemail.net",
+  "spam4.me",
+  "superrito.com",
+  "armyspy.com",
+  "cuvox.de",
+  "dayrep.com",
+  "einrot.com",
+  "fleckens.hu",
+  "gustr.com",
+  "jourrapide.com",
+  "rhyta.com",
+  "teleworm.us",
+];
